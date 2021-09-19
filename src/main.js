a2a694f6895d1e29a925b289fd3d4c6d329addfc
Mario Muniz <mario462@gmail.com>
2021-09-19 01:42:40 -0400

    Receiving data from backend

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import axios from 'axios'
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:8000/';  // the FastAPI backend
 
 Vue.config.productionTip = false
 
